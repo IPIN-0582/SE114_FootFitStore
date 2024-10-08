4f6e8b0e15cc553163810e9878a155941e14c050
IPIN-0582 <quanghuyst2004@gmail.com>
2024-10-08 21:15:36 +0700

    Lien ket lai login vs onboard

--- app/src/main/java/com/example/footfitstore/OnBoard3Activity.java
+++ app/src/main/java/com/example/footfitstore/OnBoard3Activity.java
@@ -1,6 +1,8 @@
 package com.example.footfitstore;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class OnBoard3Activity extends AppCompatActivity {
 
+    Button btnOnboard3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_on_board3);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        initializeComponent();
+
+    }
+    private void initializeComponent() {
+
+        btnOnboard3 =findViewById(R.id.btnOnboard3);
+
+        btnOnboard3.setOnClickListener(view -> {
+            startActivity(new Intent(OnBoard3Activity.this, LoginActivity.class));
         });
+
     }
 }
