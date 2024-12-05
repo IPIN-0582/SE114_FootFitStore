2c863419488b6bad131f540aae1a654da3a67cee
Phan Duc Minh <23520950@gm.uit.edu.vn>
2024-12-05 23:48:51 +0700

    Add QR Code Payment Method

--- app/src/main/java/com/example/footfitstore/activity/PaymentActivity.java
+++ app/src/main/java/com/example/footfitstore/activity/PaymentActivity.java
@@ -11,6 +11,8 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,11 +20,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.footfitstore.Api.CreateOrder;
+import com.example.footfitstore.Api.PayOsApiService;
+import com.example.footfitstore.Api.payos.CreateOrderLink;
+import com.example.footfitstore.Api.payos.CreateOrderResponseLink;
+import com.example.footfitstore.Api.payos.MinimalizeShoeDescription;
+import com.example.footfitstore.Api.zalopay.CreateOrder;
 import com.example.footfitstore.R;
+import com.example.footfitstore.Utils.OrderIdGenerator;
 import com.example.footfitstore.adapter.PaymentAdapter;
 import com.example.footfitstore.model.Cart;
 import com.example.footfitstore.model.PaymentMethod;
+import com.example.footfitstore.model.Shoe;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -40,12 +48,17 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import vn.zalopay.sdk.Environment;
 import vn.zalopay.sdk.ZaloPayError;
 import vn.zalopay.sdk.ZaloPaySDK;
 import vn.zalopay.sdk.listeners.PayOrderListener;
 
 public class PaymentActivity extends AppCompatActivity {
+    private static final int PAYMENT_TIMEOUT = 300000;
     private String paymentMethod;
     int selecteditem=-1;
     double finalprice;
@@ -60,6 +73,8 @@ public class PaymentActivity extends AppCompatActivity {
     TextView txtEmail,txtPhone,txtAddr,txtProduct,txtDelivery,txtTotal;
     ImageButton btnBack;
     Button btnCheckout;
+    List<Cart> finalCart = new ArrayList<>();
+    private ActivityResultLauncher<Intent> launcher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -101,13 +116,14 @@ public class PaymentActivity extends AppCompatActivity {
 
             loadCartData();
         }
-
+        getFinalCartList();
         txtProduct.setText("Total: $" + productPrice);
         txtDelivery.setText("Total: $" + 15.0);
         finalprice=productPrice+15.0;
         txtTotal.setText("Total: $"+finalprice);
         methodList.add(new PaymentMethod("ZaloPay", R.drawable.zalopay));
         methodList.add(new PaymentMethod("Cash on Delivery",R.drawable.cod));
+        methodList.add(new PaymentMethod("QR Code",R.drawable.ic_qr_code));
         paymentAdapter=new PaymentAdapter(R.layout.layout_payment_method,PaymentActivity.this, methodList);
         spinner.setAdapter(paymentAdapter);
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -124,8 +140,12 @@ public class PaymentActivity extends AppCompatActivity {
         btnCheckout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                if (selecteditem == 1)
+                if (selecteditem == 2)
+                {
+                    paymentMethod = "QR Code";
+                    paywithQR();
+                }
+                else if (selecteditem == 1)
                 {
                     paymentMethod = "COD";
                     paymentSuccess();
@@ -173,7 +193,27 @@ public class PaymentActivity extends AppCompatActivity {
         StrictMode.setThreadPolicy(policy);
         // ZaloPay SDK Init
         ZaloPaySDK.init(553, Environment.SANDBOX);
+        launcher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == RESULT_CANCELED) return;
+
+                    Intent data = result.getData();
+                    if (data == null) return;
+
+                    String status = data.getStringExtra("status");
+                    if (status == null) return;
+                    if (status.equals("cancelled")) {
+                        createDialog("cancel");
+                    } else if (status.equals("success")) {
+                        paymentSuccess();
+                    }
+                }
+        );
     }
+
+
+
     private void setValueActivity() {
         String userUid = mAuth.getCurrentUser().getUid();
         mDatabase.child("Users").child(userUid).addListenerForSingleValueEvent(new ValueEventListener() {
@@ -303,5 +343,81 @@ public class PaymentActivity extends AppCompatActivity {
         String userId=FirebaseAuth.getInstance().getCurrentUser().getUid();
         DatabaseReference finalOrder = FirebaseDatabase.getInstance().getReference().child("Users").child(userId).child("FinalOrder");
         finalOrder.removeValue();
+    }
+    private void paywithQR() {
+        final String CANCEL_URL = "http://cancelpayment.com";
+        final String RETURN_URL = "http://successpayment.com";
+        final String ORDER_ID_EXIST_ERR_CODE = "231";
+
+        btnCheckout.setEnabled(false); // Disable pay button to prevent multiple payments
+
+        List<MinimalizeShoeDescription> movieDescription = new ArrayList<>();
+        for (Cart cart: finalCart)
+        {
+            MinimalizeShoeDescription minimalizeShoeDescription = new MinimalizeShoeDescription(cart.getProductName(), cart.getQuantity(), cart.getSize(), cart.getPrice());
+            movieDescription.add(minimalizeShoeDescription);
+        }
+        long expiredAt = (System.currentTimeMillis() / 1000) + (PAYMENT_TIMEOUT / 1000);
+
+        int orderId = OrderIdGenerator.generateOrderId();
+
+        CreateOrderLink data = new CreateOrderLink(orderId, 2000, "Movie ticket",
+                movieDescription, CANCEL_URL, RETURN_URL, expiredAt);
+
+        Dotenv dotenv = Dotenv.configure().directory("/assets").filename("env").load();
+        PayOsApiService.payOsApiService.createPayLink(dotenv.get("PAYOS_CLIENT_ID"), dotenv.get("PAYOS_API_KEY"), data).enqueue(new Callback<CreateOrderResponseLink>() {
+            @Override
+            public void onResponse(Call<CreateOrderResponseLink> call, Response<CreateOrderResponseLink> response) {
+                btnCheckout.setEnabled(true);
+
+                if (!response.isSuccessful() || response.body() == null) {
+                    createDialog("error");
+                    return;
+                }
+
+                CreateOrderResponseLink dataResponse = response.body();
+
+                if (dataResponse.getCode().equals(ORDER_ID_EXIST_ERR_CODE)) {
+                    createDialog("error");
+                    return;
+                }
+                else if (!dataResponse.getCode().equals("00")) {
+                    createDialog("error");
+                    return;
+                }
+
+                String paymentLink = dataResponse.getData().getCheckoutUrl();
+                Intent intent = new Intent(PaymentActivity.this, PayOsActivity.class);
+                intent.putExtra("checkoutUrl", paymentLink);
+                intent.putExtra("cancelUrl", CANCEL_URL);
+                intent.putExtra("successUrl", RETURN_URL);
+                launcher.launch(intent);
+            }
+
+            @Override
+            public void onFailure(Call<CreateOrderResponseLink> call, Throwable throwable) {
+                createDialog("error");
+            }
+        });
+    }
+    private void getFinalCartList() {
+        FirebaseUser currentUser= FirebaseAuth.getInstance().getCurrentUser();
+        String userUid = currentUser.getUid();
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Users").child(userUid).child("FinalOrder");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren())
+                {
+                    Cart cart = dataSnapshot.getValue(Cart.class);
+                    finalCart.add(cart);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/footfitstore/fragment/CartFragment.java
+++ app/src/main/java/com/example/footfitstore/fragment/CartFragment.java
@@ -2,33 +2,26 @@ package com.example.footfitstore.fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.footfitstore.Api.CreateOrder;
 import com.example.footfitstore.R;
 import com.example.footfitstore.activity.MainActivity;
 import com.example.footfitstore.activity.PaymentActivity;
 import com.example.footfitstore.adapter.CartAdapter;
 import com.example.footfitstore.model.Cart;
 import com.example.footfitstore.model.Promotion;
-import com.google.android.material.bottomsheet.BottomSheetDialog;
-import com.google.firebase.Firebase;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -36,8 +29,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import org.json.JSONObject;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -49,11 +40,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-
-import vn.zalopay.sdk.Environment;
-import vn.zalopay.sdk.ZaloPayError;
-import vn.zalopay.sdk.ZaloPaySDK;
-import vn.zalopay.sdk.listeners.PayOrderListener;
 
 public class CartFragment extends Fragment {
 
